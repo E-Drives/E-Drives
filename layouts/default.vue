0eed69dc01153f16d733a951f1dd550c23b78113
Jugal <jugaldeepak@gmail.com>
2020-03-25 13:12:10 +0530

    set up nav bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,40 @@
 <template>
   <v-app dark>
+    <v-app-bar color="secondary" fixed app>
+      <v-toolbar-title></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-for="item in menu" :key="item.icon" text color="white">{{ item.title }}</v-btn>
+      </v-toolbar-items>
+      <v-app-bar-nav-icon class="hidden-md-and-up" color="white" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
+    <v-navigation-drawer right temporary fixed v-model="drawer">
+      <v-list>
+        <v-list-item v-for="item in menu" :key="item.icon">
+          <v-list-item-action-text>{{ item.title }}</v-list-item-action-text>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      drawer: false,
+      menu: [
+        { icon: "Home", title: "Home" },
+        { icon: "Section 1", title: "Section 1" },
+        { icon: "Section 2", title: "Section 2" },
+        { icon: "Section 3", title: "Section 3" }
+      ]
+    };
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
