b6a8550dc337018c53bb55996c51fcae42a85e94
Jugal <jugaldeepak@gmail.com>
2020-03-27 02:30:50 +0530

    set up tab bar for about page

--- components/about/tabbar.vue
+++ components/about/tabbar.vue
@@ -1,14 +1,23 @@
 <template>
-  <v-tabs>
-    <v-tab>About</v-tab>
-    <v-tab>Terms & Conditions</v-tab>
-    <v-tab>Privacy Policy</v-tab>
-    <v-tab>FAQ</v-tab>
-  </v-tabs>
+  <div>
+    <v-tabs background-color="primary" fixed-tabs>
+      <v-tab>About</v-tab>
+      <v-tab>Terms & Conditions</v-tab>
+      <v-tab>Privacy Policy</v-tab>
+      <v-tab>FAQ</v-tab>
+      <v-tab>Contact Us</v-tab>
+
+      <v-tab-item>About</v-tab-item>
+      <v-tab-item>Terms & Conditions</v-tab-item>
+      <v-tab-item>Privacy Policy</v-tab-item>
+      <v-tab-item>FAQ</v-tab-item>
+      <v-tab-item>Contact Us</v-tab-item>
+    </v-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'tabbar'
-}
+  name: "tabbar"
+};
 </script>
