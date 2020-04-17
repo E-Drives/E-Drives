523b9fedc490dce5308b6ebc38b09dd1da44541d
Jugal <jugaldeepak@gmail.com>
2020-04-18 02:45:34 +0530

    updated testimonials

--- components/home/edrive-testimonial.vue
+++ components/home/edrive-testimonial.vue
@@ -8,33 +8,46 @@
       </v-row>
     </v-container>
     <v-container fluid>
-      <v-carousel height="400" show-arrows-on-hover>
-        <v-carousel-item v-for="(slide, index) in slides" :key="index">
-          <v-sheet color="secondary" height="100%">
-            <v-row justify="center" align="center" class="fill-height">
-              <v-col>
-                <v-row justify="center" align="center">
-                  <v-avatar size="200">
-                    <img :src="getImage(slide.src)" width="200" height="200" />
-                  </v-avatar>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-carousel height="400" show-arrows-on-hover>
+            <v-carousel-item v-for="(slide, index) in slides" :key="index">
+              <v-sheet color="secondary" height="100%">
+                <v-row justify="center" align="center" class="fill-height">
+                  <v-col>
+                    <v-row justify="center" align="center">
+                      <v-avatar size="175">
+                        <img :src="getImage(slide.src)" width="175" height="175" />
+                      </v-avatar>
+                    </v-row>
+                    <v-row justify="center" align="center">
+                      <div
+                        :class="{ 'title': $vuetify.breakpoint.xs, 'headline': $vuetify.breakpoint.smAndUp}"
+                      >{{ slide.title }}</div>
+                    </v-row>
+                    <v-row justify="center" align="center">
+                      <div
+                        :class="{'text-body-sm': $vuetify.breakpoint.smAndUp, 'text-body': $vuetify.breakpoint.xs, 'body-1': $vuetify.breakpoint.smAndUp}"
+                      >
+                        <div class="text-center">{{ slide.text }}</div>
+                      </div>
+                    </v-row>
+                  </v-col>
                 </v-row>
-                <v-row justify="center" align="center">
-                  <div
-                    :class="{ 'body-1': $vuetify.breakpoint.xs, 'display-1': $vuetify.breakpoint.smAndUp}"
-                  >{{ slide.title }}</div>
-                </v-row>
-                <v-row justify="center" align="center">
-                  <div
-                    :class="{'title': $vuetify.breakpoint.smAndUp, 'text-body': $vuetify.breakpoint.xs}"
-                  >
-                    <span class="text-center">{{ slide.text }}</span>
-                  </div>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
-      </v-carousel>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+        <v-col cols="12" md="6">
+          <iframe
+            class="vid-responsive"
+            src="https://www.youtube.com/embed/sG2Mkx8aLUs"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </v-col>
+      </v-row>
     </v-container>
   </v-container>
 </template>
@@ -52,7 +65,28 @@
 }
 
 .text-body {
-  width: 225px;
+  width: 300px;
+}
+
+.text-body-sm {
+  width: 300px;
+}
+
+.vid-responsive {
+  width: 100%;
+  height: 100%;
+}
+
+@media all and (max-width: 960px) {
+  .vid-responsive {
+    min-height: 500px;
+  }
+}
+
+@media all and (max-width: 600px) {
+  .vid-responsive {
+    min-height: 300px;
+  }
 }
 </style>
 
